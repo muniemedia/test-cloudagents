f7cce7e0b39b451128e7f005950560abc82408a6
visin <visindev@gmail.com>
2023-10-08 18:11:26 -0500

    init

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 $chatGPT = new \classes\ChatGPT();
 $response = $chatGPT->test($_GET['input']);
